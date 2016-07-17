d85a1f7082401bb5aed1524c179c69fd1a4e042c
xuatz <xzlow10@gmail.com>
2016-07-17 19:26:32 +0800

    update main menu

--- ZooTracker/app/src/main/java/slipperytoes/zootracker/MainMenu_Activity.java
+++ ZooTracker/app/src/main/java/slipperytoes/zootracker/MainMenu_Activity.java
@@ -1,9 +1,11 @@
 package slipperytoes.zootracker;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by Thoma on 3/07/2016.
@@ -17,6 +19,18 @@ public class MainMenu_Activity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        final Context self = this;
+
+        TextView tvAnimals = (TextView) findViewById(R.id.tv_animals);
+
+        tvAnimals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO xuatz: not sure if the usage of self is unorthodox in android or not
+                Intent intent = new Intent(self, Animals_Menu_Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void openAnimalPage(View view) {
